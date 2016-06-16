36ec23c1f097488a804ab56801d284cc677cbd69
Jeff Britts <jbritts@magento.com>
2016-06-16 16:00:35 -0400

    initial commit

---
+++ Setup/InstallData.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace MagentoEse\LumaDEProducts\Setup;
+
+use Magento\Framework\Setup\InstallDataInterface;
+use Magento\Framework\Setup\ModuleContextInterface;
+use Magento\Framework\Setup\ModuleDataSetupInterface;
+
+
+    /**
+ * @codeCoverageIgnore
+ */
+class InstallData implements InstallDataInterface
+{
+
+    protected $sampleDataContext;
+    protected $storeView;
+    protected $product;
+    private $state;
+
+
+    public function __construct(\Magento\Framework\Setup\SampleData\Context $sampleDataContext,
+                                \Magento\Store\Model\Store $storeView,
+                                \Magento\Catalog\Model\ProductFactory $productFactory,
+                                \Magento\Framework\App\State $state)
+    {
+
+        try{
+            $state->setAreaCode('adminhtml');
+        }
+        catch(\Magento\Framework\Exception\LocalizedException $e){
+            // left empty
+        }
+
+        $this->config = require 'Config.php';
+        $this->fixtureManager = $sampleDataContext->getFixtureManager();
+        $this->csvReader = $sampleDataContext->getCsvReader();
+        $this->storeView = $storeView;
+        $this->productFactory = $productFactory;
+    }
+
+    public function install(ModuleDataSetupInterface $setup, ModuleContextInterface $context)
+    {
+        //get view id from view code
+        $_viewId = $this->storeView->load($this->config['viewCode'])->getStoreId();
+
+        //get category label translations
+        $_fileName = $this->fixtureManager->getFixture('MagentoEse_LumaDEProducts::fixtures/Products.csv');
+        $_rows = $this->csvReader->getData($_fileName);
+
+        $_header = array_shift($_rows);
+
+        foreach ($_rows as $_row) {
+            $_product = $this->productFactory->create();
+            $_data = [];
+            foreach ($_row as $_key => $_value) {
+                $_data[$_header[$_key]] = $_value;
+            }
+            $_row = $_data;
+            echo $_row['sku'];
+            $_product->load($_product->getIdBySku($_row['sku']));
+            $_product->setStoreId($_viewId);
+            $_product->setName($_row['name']);
+            $_product->setData('description',$_row['description']);
+            $_product->save();
+            unset($_product);
+        }
+    }
+}
